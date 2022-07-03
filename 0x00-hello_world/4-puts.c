90e3d2197279c4ed17cb4e3d0274b5e6a98db259
“abidemisam” <abidemiolayiwola23@gmail.com>
2022-07-03 11:46:27 -0700

    c code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 
-/**
- * this is a put function
- */
+/* this is for C code */
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n);
+	puts("Programming is like building a multilingual puzzle);
 	return (0);
 }
