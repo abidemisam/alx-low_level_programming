588968271c5aa70809c9d0f00dad80d09b1f20ab
“abidemisam” <abidemiolayiwola23@gmail.com>
2022-07-03 22:49:41 -0700

    compiler

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ * main - entry point 
+ * main: void
+ * return: always zero
+ */
+int main(void);
+/* void is the answer */
+{
+       	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
